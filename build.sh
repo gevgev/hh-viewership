0516ddf194b880226e6967fa035838b5ffb8f2ef
gevgev <gevgev@yahoo.com>
2016-07-10 20:25:47 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -x
+
+go build -v github.com/gevgev/cdwdatagetter
+
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+go build -v github.com/gevgev/aws-s3-uploader
+
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo 'Success'
---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+
+rm -fr cdw-data-reports/
+rm -fr cdw_downloads_logs/
+rm -fr event/
+rm -fr input_compressed_cdw_data
+rm data_downloader_tracker.txt
