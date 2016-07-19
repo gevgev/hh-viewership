db15600f6cd4759cf93edbd565cd697670cae4fb
gevgev <gevgev@yahoo.com>
2016-07-19 14:31:05 -0700

    added aws data pipeline build command

---
+++ build-data-pipeline.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -x
+
+mkdir build-data-pipeline
+
+cd build-data-pipeline/
+
+echo "Build cdwdatagetter"
+GOOS=linux go build -v github.com/gevgev/cdwdatagetter
+
+rc=$?; if [[ $rc != 0 ]]; then 
+	echo "Build failed: cdwdatagetter"
+	cd ..
+	exit $rc; 
+fi
+
+echo "Build aws-s3-uploader"
+GOOS=linux go build -v github.com/gevgev/aws-s3-uploader
+
+rc=$?; if [[ $rc != 0 ]]; then 
+	echo "Build failed: aws-s3-uploader"
+	cd ..
+	exit $rc; 
+fi
+
+echo "Copying script and mso list"
+cp ../run-ubuntu-hh-viewership.sh run.sh
+cp ../mso-list-full.csv mso-list.csv
+cp ../loop.sh loop.sh
+
+echo "Pushing to S3"
+
+aws s3 cp . s3://daap-pipeline/hh-viewership --recursive
+
+echo 'Success'
+cd ..
